11ea5dd6c219916bee575b774862fe3eafcc4485
Eunseo Kim <eunseo@hanyang.ac.kr>
2022-05-03 20:37:51 +0900

    Upload 2022.5.3

---
+++ String/camelcase_easy_KES.py
@@ -0,0 +1,18 @@
+import os
+
+def camelcase(s):
+    cnt = 0
+    for c in s:
+        if c.isupper(): cnt += 1
+    return cnt+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
