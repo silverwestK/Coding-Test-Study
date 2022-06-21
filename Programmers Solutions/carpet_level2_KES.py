b3fde43382de6ce16cef42eb1693423a3a6f1257
eunseo <eunseo@hanyang.ac.kr>
2022-06-21 23:17:54 +0900

    Add solution for 9th study (2022.6.27)

---
+++ Programmers Solutions/carpet_level2_KES.py
@@ -0,0 +1,5 @@
+def solution(brown, yellow):
+    grids = brown + yellow
+    for i in range(round(grids**0.5), 2, -1):
+        if grids%i == 0 and (grids//i-2)*(i-2) == yellow :
+            return [grids//i, i]
